572446f009b72b926dbc7be5d14db6fa995e1fa2
gshah2020 <gaurang.r.shah@gmail.com>
2024-02-29 02:01:44 -0500

    wip: blur-image

---
+++ components/blur-image.tsx
@@ -0,0 +1,33 @@
+"use-client"
+import Image from 'next/image';
+
+import {
+  dataUrl,
+  PlaceholderValue,
+} from '@/lib/utils';
+
+import { AspectRatio } from './ui/aspect-ratio';
+
+
+
+type BlurImageProps = {
+  src: string;
+  alt: string;
+};
+
+export function BlurImage({ src, alt }: BlurImageProps) {
+  return (
+    <div className="relative">
+      <AspectRatio ratio={16 / 9} className='w-full h-full'>
+        <Image
+          src={src}
+          alt={alt}
+          fill
+          objectFit="contain"
+          // @SEE: shimmer effect: lib/utils.ts
+          placeholder={dataUrl as PlaceholderValue}
+        />
+      </AspectRatio>
+    </div>
+  );
+}
